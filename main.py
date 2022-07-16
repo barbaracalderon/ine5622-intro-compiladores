5504307babb5d95fa2fed945f73b486689df84c0
gabenasci <gabenascimento1@gmail.com>
2022-07-16 18:10:31 -0300

    Separação dos arquivos Lexico e Sintatico

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append("../..")
+
+from sintatica import *
+from lexica import *
+
+# Leitura do arquivo de dados
+def read_data():
+    with open("dados_entrada.txt", mode="r", encoding="utf-8") as file:
+        dados = file.read()
+        return dados
+
+# data = input('Digite seu token: \n')
+data = read_data()
+
+analyse_lex(data)
+
+parse(data)
--- sintatica.py
+++ sintatica.py
@@ -1,111 +1,5 @@
-
-from ply.lex import lex
 from ply.yacc import yacc
-
-reserved = {
-    'def'       :   'DEF',
-    'int'       :   'INT',
-    'float'     :   'FLOAT',
-    'string'    :   'STRING',
-    'break'     :   'BREAK',
-    'print'     :   'PRINT',
-    'read'      :   'READ',
-    'return'    :   'RETURN',
-    'if'        :   'IF',
-    'else'      :   'ELSE',
-    'for'       :   'FOR',
-    'new'       :   'NEW',
-    'null'      :   'NULL',
-    'call'      :   'CALL'
-}
-
-tokens = ('LPAREN', 'RPAREN', 'LCHAVES', 'RCHAVES', 'VIRGULA', 'PONTO_VIRGULA', 'LCOLCHETES', 'RCOLCHETES', 'ATRIBUICAO', 'MENOR',
-          'MENORIGUAL', 'IGUAL','DIFERENTE', 'MAIOR', 'MAIORIGUAL', 'SOMA', 'SUBTRACAO', 'MULTIPLICACAO', 'DIVISAO', 'RESTO', 'IDENT',
-          'INT_CONSTANT', 'FLOAT_CONSTANT', 'STRING_CONSTANT', 'DEF', 'INT', 'FLOAT', 'STRING', 'BREAK', 'PRINT', 'READ', 'RETURN',
-          'IF', 'ELSE', 'FOR', 'NEW', 'NULL', 'CALL')
-        #   + list(reserved.values())
-
-t_ignore            = ' \t'
-t_DEF               = r'def'
-t_INT               = r'int'
-t_FLOAT             = r'float'
-t_STRING            = r'string'
-t_BREAK             = r'break'
-t_PRINT             = r'print'
-t_READ              = r'read'
-t_RETURN            = r'return'
-t_IF                = r'if'
-t_ELSE              = r'else'
-t_FOR               = r'for'
-t_NEW               = r'new'
-t_NULL              = r'null'
-t_CALL              = r'call'
-t_LPAREN            = r'\('
-t_RPAREN            = r'\)'
-t_LCHAVES           = r'\{'
-t_RCHAVES           = r'\}'
-t_VIRGULA           = r','
-t_PONTO_VIRGULA     = r';'
-t_LCOLCHETES        = r'\['
-t_RCOLCHETES        = r'\]'
-t_ATRIBUICAO        = r'='
-t_MENOR             = r'<'
-t_MAIOR             = r'>'
-t_MENORIGUAL        = r'<='
-t_IGUAL             = r'=='
-t_DIFERENTE         = r'!='
-t_MAIORIGUAL        = r'>='
-t_SOMA              = r'\+'
-t_SUBTRACAO         = r'-'
-t_MULTIPLICACAO     = r'\*'
-t_DIVISAO           = r'/'
-t_RESTO             = r'%'
-
-def t_IDENT(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    # if t.value in reserved:# Verifica se está nas palavras reservadas.
-    #     t.type = reserved[t.value]
-    t.type = reserved.get(t.value, 'IDENT')
-    return t
-
-def t_FLOAT_CONSTANT(t):
-    r'\d+[.]\d*|\d*[.]\d+'    # digito+.digito* | digito*.digito+
-    t.value = float(t.value)
-    return t
-
-def t_INT_CONSTANT(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
-
-def t_STRING_CONSTANT(t):
-    #r'\"([^\\\n]|(\\.))*?\"'    # "letra*"
-    r'\"\w\"'
-    return t
-
-# Definição de uma regra para encontrar o número da linha.
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
-
-# A string containing ignored characters (spaces and tabs)
-# Vide t_ignore
-
-# Error handling rule
-def t_error(t):
-    print(f'Illegal character {t.value[0]!r}')
-    t.lexer.skip(1)
-
-# Build the lexer object
-lexer = lex()
-    
-# data = 'def funcao ( int A , int B , float C , string Z ) { break ; }'
-
-# lexer.input(data)
-
-# for tok in lexer:
-#     print(tok)
-
+from lexica import *
 # --- Parser
 
 def p_program(p):
@@ -338,6 +232,7 @@ def p_c(p):
             | epsilon
     '''
 
+
 def p_epsilon(p):
     'epsilon :'
 
@@ -346,7 +241,7 @@ class Erros:
         self.sem_erros = sem_erros
     
     def set_erros(self):
-        self.sem_erros = not self.sem_erros
+        self.sem_erros = False
 
 e = Erros(True)
 
@@ -364,8 +259,11 @@ parser = yacc()
 # Parse an expression
 #ast = parser.parse('def nomefuncao ( int A , int B ) { break ; }')
 #ast = parser.parse('call def funcao ( int A , int B ) { }')
-ast = parser.parse('def funcao ( int A , int B , float C , string Z ) { break ; }')
+# ast = parser.parse(data())
 
-if (e.sem_erros):
-    print('Não foram detectados erros sintáticos')
-
+def parse(data):
+    # ast = parser.parse(data)
+    parser.parse(data)
+    if (e.sem_erros):
+        print('Não foram detectados erros sintáticos')
+    
