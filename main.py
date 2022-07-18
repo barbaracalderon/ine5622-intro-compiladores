42e4785e21bce35d9ccc2d28cd900c2e5e181e85
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2022-07-17 21:04:08 -0300

    Ajuste de formatação no main (tirar erros acusados pelo Pycharm)

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ sys.path.append("../..")
 from sintatica import *
 from lexica import *
 
+
 # Leitura do arquivo de dados
 def read_data():
     with open("dados_entrada.txt", mode="r", encoding="utf-8") as file:
         dados = file.read()
         return dados
+
 
 # data = input('Digite seu token: \n')
 data = read_data()
