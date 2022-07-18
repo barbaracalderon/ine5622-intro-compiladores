2dee7937d1585235f568aeb85a5735faeee08252
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2022-07-18 06:31:25 -0300

    Editei o arquivo antigo "lex" com a tabela de simbolos (depois atualizar oficial lexica)

--- lex.py
+++ lex.py
@@ -1,14 +1,5 @@
-#!/usr/bin/python
-import sys
-sys.path.append("../..")
-
-from ply import *
-
-'''
-if "cmm" not in sys.argv[0]:
-    print ("usage : cmm inputfile")
-    raise SystemExit
-'''
+from ply.lex import lex
+import re
 
 reserved = {
     'def'       :   'DEF',
@@ -17,19 +8,43 @@ reserved = {
     'string'    :   'STRING',
     'break'     :   'BREAK',
     'print'     :   'PRINT',
+    'call'      :   'CALL',
     'read'      :   'READ',
     'return'    :   'RETURN',
     'if'        :   'IF',
     'else'      :   'ELSE',
     'for'       :   'FOR',
     'new'       :   'NEW',
-    'null'      :   'NULL'
+    'null'      :   'NULL',
 }
 
-tokens = ['LPAREN', 'RPAREN', 'LCHAVES', 'RCHAVES', 'VIRGULA', 'PONTO_VIRGULA', 'LCOLCHETES',
-          'RCOLCHETES', 'ATRIBUICAO', 'MENOR', 'MENORIGUAL', 'IGUAL', 'DIFERENTE', 'MAIOR',
-          'MAIORIGUAL', 'SOMA', 'SUBTRACAO', 'MULTIPLICACAO', 'DIVISAO', 'RESTO', 'IDENT',
-          'INT_CONSTANT', 'FLOAT_CONSTANT', 'STRING_CONSTANT'] + list(reserved.values())
+tokens = [
+        'IDENT',
+        'FLOAT_CONSTANT',
+        'INT_CONSTANT',
+        'STRING_CONSTANT',
+        'LPAREN',
+        'RPAREN',
+        'LCHAVES',
+        'RCHAVES',
+        'VIRGULA',
+        'PONTO_VIRGULA',
+        'LCOLCHETES',
+        'RCOLCHETES',
+        'ATRIBUICAO',
+        'MENOR',
+        'MENORIGUAL',
+        'IGUAL',
+        'DIFERENTE',
+        'MAIOR',
+        'MAIORIGUAL',
+        'SOMA',
+        'SUBTRACAO',
+        'MULTIPLICACAO',
+        'DIVISAO',
+        'RESTO',
+        ] + list(reserved.values())
+
 
 t_ignore            = ' \t'
 t_DEF               = r'def'
@@ -45,6 +60,7 @@ t_ELSE              = r'else'
 t_FOR               = r'for'
 t_NEW               = r'new'
 t_NULL              = r'null'
+t_CALL              = r'call'
 t_LPAREN            = r'\('
 t_RPAREN            = r'\)'
 t_LCHAVES           = r'\{'
@@ -55,6 +71,7 @@ t_LCOLCHETES        = r'\['
 t_RCOLCHETES        = r'\]'
 t_ATRIBUICAO        = r'='
 t_MENOR             = r'<'
+t_MAIOR             = r'>'
 t_MENORIGUAL        = r'<='
 t_IGUAL             = r'=='
 t_DIFERENTE         = r'!='
@@ -68,84 +85,54 @@ t_RESTO             = r'%'
 
 def t_IDENT(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
-    if t.value in reserved:  # Verifica se está nas palavras reservadas.
-        t.type = reserved[t.value]
+    t.type = reserved.get(t.value, 'IDENT')
     return t
-
 
 def t_FLOAT_CONSTANT(t):
     r'\d+[.]\d*|\d*[.]\d+'    # digito+.digito* | digito*.digito+
     t.value = float(t.value)
     return t
 
-
 def t_INT_CONSTANT(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-
 def t_STRING_CONSTANT(t):
-    #r'\"([^\\\n]|(\\.))*?\"'    # "letra*"
-    r'"\w\"'
+    r'\"\w\"'
     return t
 
-'''
 # Definição de uma regra para encontrar o número da linha.
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
-'''
-
-# A string containing ignored characters (spaces and tabs)
-# Vide t_ignore
-
-literals = ["'", '"']
-
-# def t_squotation(t):    # Single quotation
-#     r"\'"
-#     t.type = "'"
-#     return t
-#
-# def t_dquotation(t):    # Double quotation
-#     r'"'
-#     t.type = '"'
-#     return t
 
 # Error handling rule
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+    print(f'Illegal character {t.value[0]!r}')
     t.lexer.skip(1)
 
-'''
-if __name__ == '__main__':
-     lex.runmain()
-'''
-
-lexer = lex.lex()
-print('Começando leitura dos dados de entrada...')
-
-# Test it out
-# data = "{   int SM[12.3]; SM[123] = A + B;   &SM[tartaruga] = B * C;    return;    }"
 
 
-def read_data():
-    with open("dados_entrada.txt", mode="r", encoding="utf-8") as file:
-        dados = file.read()
-        return dados
+# Build the lexer object
+lexer = lex()
 
-
-data = read_data()
-print(f'Dados de entrada: {data}')
-
-# Give the lexer some input
-lexer.input(data)
-
-# Tokenize
-while True:
-    tok = lexer.token()
-    if not tok:
-        break  # No more input
-    print(tok)
-
-
+def analyse_lex(data):
+    lexer.input(data)
+    tabela_simbolo = []
+    tabela_check = []
+    cont = 1
+    print('\nANÁLISE LÉXICA:\n')
+    for tok in lexer:
+        print(f'tok: {tok}')
+        if tok.type == 'IDENT':
+            tup = (tok.value, cont)
+            if tup[0] not in tabela_check:
+                cont += 1
+                tabela_check.append(tup[0])
+                tabela_simbolo.append(tup)
+    print('\n')
+    print('TABELA DE SÍMBOLOS:')
+    for tup in tabela_simbolo:
+        print(tup)
+    return tabela_simbolo
